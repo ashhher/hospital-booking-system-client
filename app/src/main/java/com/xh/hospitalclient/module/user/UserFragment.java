002227323aef254581b34ddc65e2e3f578370afd
huuuuuuuuux <huxley.xiaohan@gmail.com>
2020-05-04 23:59:12 +0800

    个人信息显示

--- app/src/main/java/com/xh/hospitalclient/module/user/UserFragment.java
+++ app/src/main/java/com/xh/hospitalclient/module/user/UserFragment.java
@@ -11,21 +11,34 @@ import android.support.v4.app.Fragment;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 
+import com.xh.hospitalclient.MyApplication;
 import com.xh.hospitalclient.R;
+import com.xh.hospitalclient.model.UserInfo;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class UserFragment extends Fragment {
 
-//    @BindView(R.id.tv_user)
-    TextView tvUser;
+    @BindView(R.id.tv_user_name)
+    TextView tvUserName;
+    @BindView(R.id.tv_user_age)
+    TextView tvUserAge;
+    @BindView(R.id.tv_user_sex)
+    TextView tvUserSex;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_user, container, false);
-//        ButterKnife.bind(this, view);
-//        tvUser.setText("THIS IS USER");
+        ButterKnife.bind(this, view);
+
+        String userName = UserInfo.get(MyApplication.getInstance()).getUserName();
+        int userAge = UserInfo.get(MyApplication.getInstance()).getUserAge();
+        String userSex = UserInfo.get(MyApplication.getInstance()).getUserSex()? "男" : "女";
+
+        tvUserName.setText(userName);
+        tvUserAge.setText(userAge+"");
+        tvUserSex.setText(userSex);
         return view;
     }
 }
